9a481b55f7da0e9ad46eede10d5c4a03a1001ef4
eddie <116ekg@gmail.com>
2016-03-19 17:44:25 -0700

    Initial Commit

---
+++ retro-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retro-calculator
+//
+//  Created by Eddie Keller on 3/19/16.
+//  Copyright © 2016 Eddie Keller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
